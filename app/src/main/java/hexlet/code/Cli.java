ee22e17fc8c40fa921ef69e52519f2a786357f6d
VETA <vinakova.vita@mail.ru>
2022-10-31 11:22:02 +0300

    checkstyle

--- app/src/main/java/hexlet/code/Cli.java
+++ app/src/main/java/hexlet/code/Cli.java
@@ -3,7 +3,7 @@ package hexlet.code;
 import java.util.Scanner;
 
 public class Cli {
-    static public void greeting() {
+    static void greeting() {
         Scanner input = new Scanner(System.in);
         System.out.print("May I have your name? ");
         String name = input.nextLine();
