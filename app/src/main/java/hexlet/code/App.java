b41073d27fb45cac1219226f9dd5ae631b861aba
VETA <vinakova.vita@mail.ru>
2022-11-05 21:21:52 +0300

    progression

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -6,7 +6,7 @@ public class App {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         System.out.println("Please enter the game number and press Enter.");
-        System.out.println("1 - Greet\n2 - Even\n3 - Calc\n4 - GCD\n0 - Exit");
+        System.out.println("1 - Greet\n2 - Even\n3 - Calc\n4 - GCD\n5 - Progression\n0 - Exit");
         int choice = input.nextInt();
         System.out.println("Your choice: " + choice);
         switch (choice) {
@@ -21,6 +21,9 @@ public class App {
                 break;
             case 4:
                 Nod.run();
+                break;
+            case 5:
+                Progression.run();
                 break;
             default:
                 break;
---
+++ app/src/main/java/hexlet/code/Progression.java
@@ -0,0 +1,47 @@
+package hexlet.code;
+
+import java.time.temporal.ChronoUnit;
+import java.util.Random;
+import java.util.StringJoiner;
+
+public class Progression {
+    private
+    static String greetingLine = "What number is missing in the progression?";
+    static int newQuestion() {
+        Random random = new Random();
+        int currentElement = random.nextInt(100);
+        int elementsNumber = random.nextInt(5, 10);
+        int hiddenElementNumber = random.nextInt(elementsNumber);
+        int progressionStep = random.nextInt(1, 10);
+
+        String progressionLine = Integer.toString(currentElement);
+
+        for (int i = 0; i < elementsNumber; i++) {
+            if (i == hiddenElementNumber) {
+                progressionLine += " ..";
+                currentElement += progressionStep;
+                Engine.rightAnswer = Integer.toString(currentElement);
+                continue;
+            }
+            currentElement += progressionStep;
+            progressionLine += " " + Integer.toString(currentElement);
+        }
+        Engine.question = progressionLine;
+        return 0;
+    }
+
+    public
+    static int run() {
+        Engine.greeting(greetingLine);
+        int roundsNumber = 3;
+        for (int i = 0; i < roundsNumber; i++) {
+            Engine.gameRound(newQuestion());
+            if (Engine.roundResult == false) {
+                System.out.println("Let's try again, " + Engine.name + "!");
+                return 0;
+            }
+        }
+        System.out.println("Congratulations, " + Engine.name + "!");
+        return 0;
+    }
+}
